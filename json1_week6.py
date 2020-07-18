b7d5a67e78662665013793f1406d4a20ce09d286
Chakradhar Rao <49837007+chakri3899@users.noreply.github.com>
2020-07-19 04:21:37 +0530

    Update json1_week6.py

--- json1_week6.py
+++ json1_week6.py
@@ -1,3 +1,7 @@
+#Problem: Exrtacting data from JSON
+#visit this url for the problem - https://www.py4e.com/tools/python-data/?PHPSESSID=23736cf4e1a89ea59a71ff0c28f49979
+
+
 import urllib.request, urllib.parse, urllib.error
 import json
 
