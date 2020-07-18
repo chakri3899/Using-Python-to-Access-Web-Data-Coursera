28a5a0eb1b725056490b4c6ff925cb4573402801
Chakradhar Rao <49837007+chakri3899@users.noreply.github.com>
2020-07-19 04:15:08 +0530

    Update urllinks1_week4.py

--- urllinks1_week4.py
+++ urllinks1_week4.py
@@ -1,3 +1,7 @@
+#Problem: Scraping numbers in html using beautiful soup
+# visit this url for problem - https://www.py4e.com/tools/python-data/?PHPSESSID=37e7f9f2d7b9cfa756d62a061344a170
+
+
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
